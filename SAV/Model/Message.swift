bd04fbe45a53ee0092c03baf8f7bf5a4dc81e1ba
Ray Tso <raytso790211@gmail.com>
2018-05-14 15:31:44 +0800

    created 'Message' & 'User' struct

---
+++ SAV/Model/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  SAV
+//
+//  Created by Ray Tso on 2018/5/13.
+//  Copyright © 2018 Ray Tso. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let message: String
+    let sender: User
+}
---
+++ SAV/Model/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  SAV
+//
+//  Created by Ray Tso on 2018/5/13.
+//  Copyright © 2018 Ray Tso. All rights reserved.
+//
+
+import Foundation
+
+struct User {
+    let name: String
+    let avatarUrl: URL
+}
